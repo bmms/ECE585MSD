50cdf88cd7576d8fd0254bfc0097eb5957cef888
Maddy2107 <ssanjay@pdx.edu>
2014-03-14 12:58:50 -0700

    Create Globalvar_final.v

---
+++ Globalvar_final.v
@@ -0,0 +1,42 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: Portland State University - ECE 585: Microprocessor System Design
+// Engineer: Sanjay Sharma and Mayur Desadla
+// 
+// Create Date: 03/03/2014 
+// Design Name: L1 cache design
+// Module Name:  
+// Project Name: Cache Simulator
+// Target Devices: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 1.0
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+
+`define EOF 32'hFFFF_FFFF
+`define Null 0
+`define Max_line_length 1000
+
+////////////////////////////////////////////////
+///////////// General declarations /////////////
+////////////////////////////////////////////////
+
+integer TotalOperations = 0;
+integer CacheReads = 0;
+integer CacheWrites = 0;
+integer CacheFetches = 0;
+integer TLBReads=0;
+integer TLBWrites=0;
+integer TLB_HitCount=0;
+integer TLB_MissCount=0;
+real L1_HitRatio = 0.0;
+real L1_MissRatio = 0.0;
+integer L1_HitCount = 0;
+integer L1_MissCount = 0;
+integer file;
+integer report_file;
+integer report_file_TLB;
